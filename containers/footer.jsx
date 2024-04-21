bb0c8d4e59e48b1a83520d38d8cc561cfbcbebd2
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-04-21 14:42:55 +0100

    footer

---
+++ containers/footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+  return (
+    <div className="flex flex-col fixed bottom-0 w-full justify-center items-center h-[80px] px-[15vw] border-t backdrop-filter backdrop-blur-xl">
+        <p className="text-primaryText py-4 text-bold text-[18px]">Crowd Funding dApp @ 2024</p>
+    </div>
+  )
+}
+
+export default Footer
