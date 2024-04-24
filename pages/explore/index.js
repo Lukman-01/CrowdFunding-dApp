3dd6960feff765b23c7adaf84307407ce127f404
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-04-24 07:46:11 +0100

    add

---
+++ pages/explore/index.js
@@ -0,0 +1,10 @@
+import ExploreContainer from "@/containers/explore"
+
+export default function Explore() {
+    return (
+      <ExploreContainer />
+    )
+  }
+  
+  Explore.title = "Explore";
+  Explore.description = "Crowd Funding dApp, a platform to support great projects";
