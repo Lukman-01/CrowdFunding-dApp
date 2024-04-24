cf420048e90a6e68dea0f9b9f6482fbb6129be95
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-04-24 23:13:25 +0100

    etherscan

--- SmartContract/hardhat.config.js
+++ SmartContract/hardhat.config.js
@@ -1,16 +1,18 @@
 require('@nomiclabs/hardhat-waffle');
 require("dotenv").config({ path: ".env" });
 
-const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-
-
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.7',
   networks: {
     sepolia: {
-      url: ALCHEMY_HTTP_URL,
+      url: "https://rpc.ankr.com/eth_sepolia",
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
-};
+
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API
+    }
+  }
+}
